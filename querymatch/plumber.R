09ba06a36b66a0813ab1082d1c9bfe1bb1a705db
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2023-04-11 13:47:41 -0400

    API IS NOT WORKING IN POSITTTTT
    But works perfectly locally

--- querymatch/plumber.R
+++ querymatch/plumber.R
@@ -1,12 +1,12 @@
 
 library(plumber)
-library(tm)
-library(slam)
 source("clean_query.R")
 source("generate_tdm.R")
 source("calculate_similarity.R")
-querytbl <- readRDS("select_querytbl.rds")
-select_tdm <- readRDS("select_tdm_model.rds")
+library(tm)
+library(slam)
+querytbl <- readRDS("querytbl.rds")
+select_tdm <- readRDS("tdm_model.rds")
 
 #* @apiTitle Query Match
 #* @apiDescription Discover queries similar to yours.
@@ -21,19 +21,60 @@ function(msg = "") {
 #* Return the top N of querytbl matching query
 #* @param querytext The SQL query seeking its match. 
 #* @param n The N top results to return.
-#* @post /querymatch
+#* @post /qmatch
 function(querytext, n = 10) {
   
   # clean for cosine similarity
   nq <- clean_query(querytext)
   nqtdm <- generate_tdm_model(nq, custom_dictionary = Terms(select_tdm))
-  res <- calculate_similarity(select_tdm, nqtdm)
+  cosims <- calculate_similarity(select_tdm, nqtdm)
   
- response <- querytbl[res$index[order(res$cosim, decreasing = TRUE)[1:n]], ]
- response$score <- res$cosim[order(res$cosim, decreasing = TRUE)[1:n]]
+ response <- querytbl
+ response$score <- cosims$cosim
+ response <- response[order(response$score, decreasing = TRUE)[1:n], ]
 
 return(response)
  
+}
+
+#* Return the key model details
+#* @post /modeldetails
+function() {
+
+  # Create two matrices as simple_triplet_matrix objects
+  mat1 <- simple_triplet_matrix(
+    i = c(1, 2, 3),
+    j = c(1, 2, 3),
+    v = c(1, 2, 3)
+  )
+  
+  mat2 <- simple_triplet_matrix(
+    i = c(1, 2, 3),
+    j = c(1, 2, 3),
+    v = c(4, 5, 6)
+  )
+  
+  # Compute the cross-product using slam::crossprod_simple_triplet_matrix()
+  crossprod_simple_triplet_matrix(mat1, mat2)
+  
+  return(list(
+    qtbl_rows = nrow(querytbl),
+    tdm_docs = length(Docs(select_tdm)),
+    correct = cosims[76286, ],
+    slam = as.character(packageVersion('slam')),
+    clss = class(select_tdm[, 76286]),
+    dim = dim(select_tdm[, 76286]),
+    rowsum = sum(slam::row_sums(select_tdm[, 76286])),
+    colsum = slam::col_sums(select_tdm[, 76286]),
+    nq =sum(row_sums(nqtdm)),
+    nqcol =sum(col_sums(nqtdm)),
+    single_ = crossprod_simple_triplet_matrix(select_tdm[, 76286], nqtdm),
+    dotproducts = slam::row_sums(slam::crossprod_simple_triplet_matrix(select_tdm[, 76286], nqtdm)),
+    magnitudes = sqrt(slam::col_sums(nqtdm^2)) * sqrt(slam::col_sums( select_tdm[, 76286]  ^ 2))
+
+  )
+  )
+  
 }
 
 # Programmatically alter your API
