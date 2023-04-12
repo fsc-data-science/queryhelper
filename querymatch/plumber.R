e1c8d6f79486fc541ac5af46a147585c4abd1e6f
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2023-04-12 16:13:59 -0400

    Include TEST & SHIFT in case Linux issues persist

--- querymatch/plumber.R
+++ querymatch/plumber.R
@@ -3,6 +3,21 @@ library(plumber)
 source("clean_query.R")
 source("generate_tdm.R")
 source("calculate_similarity.R")
+
+# when updating to a new model 
+# update the testshift accordingly 
+# as Linux RConnect may shift sparse matrix i values by +1
+# which causes a problem in cosine similarity.
+
+# It is known that query row 76286, corresponding 
+# with column (Doc) 76286 in the trained tdm_model 
+# has a cosine similarity with the test input of 0.63 
+# update as new models are generated 
+test_row = 76286
+test_input = "select count num daily users from ethereum fact transactions"
+test_target = 0.63
+
+custom_dict <- readRDS("custom_dict.rds")
 querytbl <- readRDS("querytbl.rds")
 select_tdm <- readRDS("tdm_model.rds")
 
@@ -16,52 +31,51 @@ function(msg = "") {
     list(msg = paste0("The message is: '", msg, "'"))
 }
 
-#* Return the top N of querytbl matching query
+#* Return the top N rows for querytbl from model matching query
 #* @param querytext The SQL query seeking its match. 
 #* @param n The N top results to return.
 #* @post /querymatch
-function(querytext, n = 10) {
+function(querytext, n = 10, shift = TRUE) {
   
   # clean for cosine similarity
   nq <- clean_query(querytext)
-  nqtdm <- generate_tdm_model(nq, custom_dictionary = Terms(select_tdm))
+  nqtdm <- generate_tdm_model(nq, custom_dictionary = custom_dict)
+  
+  if(shift){
+    nqtdm$i <- as.integer(nqtdm$i - 1)
+  }
+  
   cosims <- calculate_similarity(select_tdm, nqtdm)
   
   topn = order(cosims$cosim, decreasing = TRUE)[1:n]
   
-return(
-  data.frame(
-    index = topn,
-    cosim = cosims$cosim[topn]
-  )
+  res_ = querytbl[topn, ]
+  res_$score <- cosims$cosim[topn]
   
+return(
+  res_
 )
  
 }
 
 #* Return the key model details
-#* @post /modeldetails
-function() {
+#* @post /testshift 
+function(shift = FALSE) {
 
-  nq <- clean_query('select count num daily users from ethereum fact transactions')
-  nqtdm <- generate_tdm_model(nq, custom_dictionary = Terms(select_tdm))
-  cosims <- calculate_similarity(select_tdm, nqtdm)
-  dot_products = slam::row_sums(crossprod_simple_triplet_matrix(select_tdm, nqtdm))
+  nq <- clean_query(test_input)
+  nqtdm <- generate_tdm_model(nq, custom_dictionary = custom_dict)
+  if(shift){
+    nqtdm$i <- as.integer(nqtdm$i - 1)
+  }
+  mat1 <- select_tdm[, test_row]
   
-mat1 <- select_tdm[, 76286]
-mat2 <- nqtdm 
-
-  return(list(
-    mean = mean(dot_products),
-    med = median(dot_products),
-    n0 = sum(dot_products == 0),
-    max = max(dot_products),
-    meanrr = mean(row_sums(select_tdm)),
-    meancc = mean(col_sums(select_tdm)),
-    testcs = calculate_similarity(mat1, mat2),
-    testdp = slam::row_sums(crossprod_simple_triplet_matrix(mat1, mat2))
-  )
-  )
+   x = round(calculate_similarity(mat1, nqtdm)$cosim, 2)
+   
+  if(x == test_target){
+    return(TRUE)
+  } else { 
+    return(FALSE)
+    }
   
 }
 
